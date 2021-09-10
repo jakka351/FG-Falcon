ebd3fe5b5bd17bd22373b62b90056d6e02c90cd1
J.L <57064943+jakka351@users.noreply.github.com>
2021-09-10 21:55:57 +1000

    Delete AsBuiltData/scripts directory

--- AsBuiltData/scripts/README.md
+++
@@ -1,9 +0,0 @@
-# ForsCan-Utils Scripts
-
-## set up can0
-  - `MidSpeed Can @ 125 KBPS`
-  -
-## usage
-    dont use these
-
-
--- AsBuiltData/scripts/boobs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-while true; do cansend can0 309#49204c494b45000000 && sleep 1 &&  cansend can0 309#424f4f4249455300; sleep 1.000; done 
