f3f1ac368a4dd98d1bf3c399bdc53e1d9005220c
jakka351 <57064943+jakka351@users.noreply.github.com>
2021-11-18 02:07:45 +1100

    Create EnableBeltMinder.sh

---
+++ resources/software/jakka351/fdim_text/bash/EnableBeltMinder.sh
@@ -0,0 +1,17 @@
+###################################
+#canbus.com.au jakka351
+# EnableBeltMinder
+###################################
+#!/bin/bash
+cansend can0 720#043B038D0E000000
+cansend can0 728#037F3B2200000000
+cansend can0 720#0210870000000000
+cansend can0 728#0250870000000000
+cansend can0 720#043B038D0E000000
+cansend can0 728#027B030000000000
+cansend can0 720#0322D10000000000
+cansend can0 728#0462D10087000000
+cansend can0 720#0210810000000000
+cansend can0 728#0250810000000000
+cansend can0 720#0211010000000000
+cansend can0 728#0251010000000000
