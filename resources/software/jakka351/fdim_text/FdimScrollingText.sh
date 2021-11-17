f993740a186877e8c845fa7f4865715a1c6cfec3
jakka351 <57064943+jakka351@users.noreply.github.com>
2021-11-18 02:03:41 +1100

    Create FdimScrollingText.sh

---
+++ resources/software/jakka351/fdim_text/FdimScrollingText.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+while true; do;
+ cansend can0 2F5#10264a616b6b6133 
+ cansend can0 2F5#21353120736e6966 
+ cansend can0 2F5#2266696e67207468 
+ cansend can0 2F5#2365206275747473 
+done
