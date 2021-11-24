399cc480331e10298c68c63c66de5c87f2bf1fcb
jakka351 <57064943+jakka351@users.noreply.github.com>
2021-11-25 02:13:41 +1100

    Create 0x309_REVERSE_SYSTEM_ACTIVE.sh

---
+++ resources/software/jakka351/fdim_text/bash/0x309_REVERSE_SYSTEM_ACTIVE.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while true;
+do
+cansend vcan0 309#5245564552534520; sleep 0.002;
+cansend vcan0 309#53595354454d2020; sleep 0.002;
+cansend vcan0 309#4143544956452020; sleep 0.002;
+done
+
+
+
+
