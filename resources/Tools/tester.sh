d2a2c48450c0251d99c765bea255d56eddab6977
Ab-Incunabulis <57064943+jakka351@users.noreply.github.com>
2023-01-04 13:45:26 +1100

    Create tester.sh

---
+++ resources/Tools/tester.sh
@@ -0,0 +1,7 @@
+cangen can0 -I 7A6 -D 023E02 -L 8 -g 2000 &
+cangen can0 -I 726 -D 023E02 -L 8 -g 2000 &
+cangen can0 -I 720 -D 023E02 -L 8 -g 2000 &
+cangen can0 -I 727 -D 023E02 -L 8 -g 2000 &
+cangen can0 -I 767 -D 023E02 -L 8 -g 2000 &
+cangen can0 -I 736 -D 023E02 -L 8 -g 2000 &
+cangen can0 -I 781 -D 023E02 -L 8 -g 2000 &
